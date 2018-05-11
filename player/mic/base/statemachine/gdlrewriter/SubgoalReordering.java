8489963c8d66ab672df5f0c8cdcf3ef0b5dc3df4
caleb <caleb@10.34.52.124>
2018-05-10 20:45:10 -0700

    Improve SubgoalReordering

--- player/mic/base/statemachine/gdlrewriter/SubgoalReordering.java
+++ player/mic/base/statemachine/gdlrewriter/SubgoalReordering.java
@@ -8,8 +8,11 @@ import java.util.Set;
 
 import org.ggp.base.util.gdl.GdlUtils;
 import org.ggp.base.util.gdl.grammar.Gdl;
+import org.ggp.base.util.gdl.grammar.GdlDistinct;
 import org.ggp.base.util.gdl.grammar.GdlLiteral;
+import org.ggp.base.util.gdl.grammar.GdlNot;
 import org.ggp.base.util.gdl.grammar.GdlPool;
+import org.ggp.base.util.gdl.grammar.GdlRelation;
 import org.ggp.base.util.gdl.grammar.GdlRule;
 import org.ggp.base.util.gdl.grammar.GdlSentence;
 import org.ggp.base.util.gdl.grammar.GdlVariable;
@@ -20,10 +23,13 @@ public class SubgoalReordering implements GdlRewriter {
 	public List<Gdl> rewrite(List<Gdl> description) {
 		List<Gdl> newDescription = new ArrayList<Gdl>();
 
-
 		for (Gdl term : description) {
 			if (term instanceof GdlRule) {
 				Gdl newTerm = reorderSubgoal((GdlRule) term);
+
+				if (!newTerm.equals(term)) {
+					System.out.println(term + " -> " + newTerm);
+				}
 				newDescription.add(newTerm);
 			} else {
 				newDescription.add(term);
@@ -40,14 +46,13 @@ public class SubgoalReordering implements GdlRewriter {
 		Set<GdlVariable> vl = new HashSet<GdlVariable>();
 		List<GdlLiteral> sl = new LinkedList<GdlLiteral>(rule.getBody());
 		while (!sl.isEmpty()) {
-			GdlLiteral ans = getBest(sl,vl);
+			GdlLiteral ans = getBest(sl, vl);
 			newBody.add(ans);
 			Set<GdlVariable> vars = GdlUtils.getVariablesSet(ans);
-			for (GdlVariable v : vars){
+			for (GdlVariable v : vars) {
 				vl.add(v);
 			}
 		}
-
 
 		return GdlPool.getRule(head, newBody);
 	}
@@ -57,12 +62,23 @@ public class SubgoalReordering implements GdlRewriter {
 		int best = 0;
 
 		for (int i = 0; i < sl.size(); i++) {
+
+			if (sl.get(i) instanceof GdlRelation) {
+				if (((GdlRelation) sl.get(i)).getName().equals(GdlPool.DOES)) {
+					best = i;
+					break;
+				}
+			}
 			int dum = unboundVarNum(sl.get(i), vl);
+			if ((sl.get(i) instanceof GdlDistinct || sl.get(i) instanceof GdlNot) && dum != 0) {
+				continue;
+			}
 			if (dum < varNum) {
 				varNum = dum;
 				best = i;
 			}
-			if (varNum == 0) break; //can't beat
+			if (varNum == 0)
+				break; // can't beat
 		}
 		GdlLiteral ans = sl.get(best);
 		sl.remove(best);
@@ -74,7 +90,4 @@ public class SubgoalReordering implements GdlRewriter {
 		vars.removeAll(vl);
 		return vars.size();
 	}
-
-
-
 }
