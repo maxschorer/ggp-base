3e63bcc5e80011c5f14d106a6b0b4850f2a734d8
caleb <caleb@10.1.10.16>
2018-05-22 09:15:09 -0700

    Use PropNet directly

--- player/mic/assignment7/Assignment7Player.java
+++ player/mic/assignment7/Assignment7Player.java
@@ -6,12 +6,10 @@ import java.util.Vector;
 import org.ggp.base.apps.player.Player;
 import org.ggp.base.util.statemachine.StateMachine;
 import org.ggp.base.util.statemachine.cache.CachedStateMachine;
-import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
 
 import mic.base.manager.EvalManager;
 import mic.base.manager.Manager;
 import mic.base.player.ThreadPlayer;
-import mic.base.statemachine.DebugStateMachine;
 import mic.base.statemachine.PropNetStateMachine;
 import mic.base.worker.MCTSWorker;
 import mic.base.worker.Worker;
@@ -37,10 +35,11 @@ public class Assignment7Player extends ThreadPlayer {
 
 	@Override
 	public StateMachine getInitialStateMachine() {
-		return new DebugStateMachine(
-				new CachedStateMachine(new PropNetStateMachine()),
-				new CachedStateMachine(new  ProverStateMachine())
-			);
+		return new CachedStateMachine(new PropNetStateMachine());
+//		return new DebugStateMachine(
+//				new CachedStateMachine(new PropNetStateMachine()),
+//				new CachedStateMachine(new  ProverStateMachine())
+//			);
 	}
 
 }
